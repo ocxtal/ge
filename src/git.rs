17a188138f5d3f1994600bbc20dd6c9ef199ef85
Hajime Suzuki <suzuki.hajime.s@gmail.com>
2022-08-09 20:31:20 +0900

    fmt

--- src/git.rs
+++ src/git.rs
@@ -336,7 +336,7 @@ mod tests {
             .grep("assert", opts!("ge --function-context -y tests/*.rs"))
             .unwrap();
         assert_eq!(output.hits.len(), 1);
-        assert!(output.hits[0].n_lines >= 3);   // workaround for old versions of git that excludes `#[test]`
+        assert!(output.hits[0].n_lines >= 3); // workaround for old versions of git that excludes `#[test]`
     }
 
     // TODO: git.apply
